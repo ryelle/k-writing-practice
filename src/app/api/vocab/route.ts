d7288e5c61c628e39559fa70af110712695a51ce
Kelly Dwan <ryelle@users.noreply.github.com>
2026-01-03 23:59:40 -0500

    Switch to Gemini for AI model

--- src/app/api/vocab/route.ts
+++ src/app/api/vocab/route.ts
@@ -1,17 +1,18 @@
 import { NextResponse } from "next/server";
 import { generateText } from "ai";
-import { createOpenAICompatible } from "@ai-sdk/openai-compatible";
+// import { createOpenAICompatible } from "@ai-sdk/openai-compatible";
+import { google } from "@ai-sdk/google";
 import LRUCache from "@/utils/cache";
 
 const cache = new LRUCache<string, string>(500);
 
 export async function POST(req: Request) {
 	const { prompt } = await req.json();
-	const args = {
-		baseURL: "https://api.upstage.ai/v1",
-		name: "upstage",
-		apiKey: process.env.UPSTAGEAI_API_KEY,
-	};
+	// const args = {
+	// 	baseURL: "https://api.upstage.ai/v1",
+	// 	name: "upstage",
+	// 	apiKey: process.env.UPSTAGEAI_API_KEY,
+	// };
 
 	const key = `vocab::${prompt}`;
 
@@ -25,8 +26,10 @@ export async function POST(req: Request) {
 	}
 
 	console.log(`Cache miss for ${prompt}.`);
+	// const model = createOpenAICompatible(args).chatModel("solar-pro2"),
+	const model = google("gemini-2.5-pro");
 	const result = await generateText({
-		model: createOpenAICompatible(args).chatModel("solar-pro2"),
+		model,
 		system: "You are a Korean language tutor for an American student learning Korean. Do not ask any follow up questions when replying.",
 		prompt: `Generate 10-20 vocabulary words in a comma-separated list for a student to use when replying to this prompt. Only return the words in Korean. Do not conjugate any verbs.\nPrompt: ${prompt}`,
 	});
